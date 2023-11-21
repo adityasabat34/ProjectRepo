9b4a96e8dfd9b6cd059a298e2c3483c115339f37
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2023-11-21 17:08:55 +0530

    Designed and Build single product screen done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       >
         <Routes>
           <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:id" element={<ProductScreen />} />
         </Routes>
       </Flex>
       <Footer />
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,92 @@
+import { Button, Flex, Grid, Heading, Image, Text } from '@chakra-ui/react';
+import { Link as RouterLink, useParams } from 'react-router-dom';
+import Rating from '../components/Rating';
+import products from '../products';
+
+const ProductScreen = () => {
+  const { id } = useParams();
+  const product = products.find((prod) => prod._id === id);
+  return (
+    <>
+      <Flex mb="5">
+        <Button
+          as={RouterLink}
+          to="/"
+          colorScheme="gray"
+          border="solid black 1px"
+        >
+          Go Back
+        </Button>
+      </Flex>
+
+      <Grid templateColumns="5fr 4fr 3fr" gap="10">
+        {/**column 1 */}
+        <Image
+          src={product.image}
+          alt={product.name}
+          borderRadius="md"
+          border="solid black 1px"
+        />
+
+        {/**column 2 */}
+
+        <Flex direction="column">
+          <Heading as="h5" fontSize="base" color="gray.500">
+            {product.brand}
+          </Heading>
+
+          <Heading as="h2" fontSize="4xl" mb="5" color="red.900">
+            {product.name}
+          </Heading>
+
+          <Rating
+            value={product.rating}
+            color="yellow.500"
+            text={`${product.numReviews} reviews`}
+          />
+
+          <Heading
+            as="h5"
+            fontSize="4xl"
+            fontWeight="bold"
+            color="teal.700"
+            my="5"
+          >
+            ₹{product.price}
+          </Heading>
+
+          <Text>{product.description}</Text>
+        </Flex>
+
+        {/**column 3 */}
+
+        <Flex direction="column">
+          <Flex justifyContent="space-between" py="2">
+            <Text>Price:</Text>
+            <Text fontWeight="bold">₹{product.price}</Text>
+          </Flex>
+
+          <Flex justifyContent="space-between" py="2" pb="10">
+            <Text>Status:</Text>
+            <Text fontWeight="bold">
+              {product.countInStock > 0 ? 'In Stock' : 'Not available'}
+            </Text>
+          </Flex>
+
+          <Button
+            bg="red.900"
+            colorScheme="blackAlpha"
+            my="2"
+            textTransform="uppercase"
+            letterSpacing="wide"
+            isDisabled={product.countInStock === 0}
+          >
+            Add to cart
+          </Button>
+        </Flex>
+      </Grid>
+    </>
+  );
+};
+
+export default ProductScreen;
