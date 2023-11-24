8399f046415883d0ccbe91cd51c791f1d1d9a989
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2023-11-24 17:21:30 +0530

    Added Styling colors for DB console messages done.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected: ${conn.connection.host}`.bgCyan.underline);
   } catch (err) {
-    console.error(`Error: ${err.message}`);
+    console.error(`Error: ${err.message}`.red.bold.underline);
     process.exit(1);
   }
 };
