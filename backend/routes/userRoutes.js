f318ffced52de4cc1393f00a37c6f59302a1dae3
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2023-12-22 18:12:08 +0530

    User Authentication setup done.

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import { authUser } from '../controllers/userController.js';
+import express from 'express';
+
+const router = express.Router();
+
+router.route('/login').post(authUser);
+
+export default router;
