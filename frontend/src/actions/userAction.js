2b23eea080aab4c075b03a0957b8de0bf4aa5fe6
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2023-12-28 17:44:03 +0530

    User Details Reducer, action and constants done.

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -7,6 +7,9 @@ import {
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_FAIL,
 } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
@@ -67,6 +70,34 @@ export const register = (name, email, password) => async (dispatch) => {
   } catch (err) {
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
+
+export const getUserDetails = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_DETAILS_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState;
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/users/profile`, config);
+
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+  } catch (err) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
       payload:
         err.response && err.response.data.message
           ? err.response.data.message
