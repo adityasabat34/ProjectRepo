e1109a1479ff903587539e2b03fdf58678038a83
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-01-30 17:29:24 +0530

    Adding product reviews on the frontend done.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,15 +5,23 @@ import {
   Heading,
   Image,
   Text,
+  FormControl,
+  FormLabel,
+  Box,
   Select,
+  Textarea,
 } from '@chakra-ui/react';
 import { Link as RouterLink, useParams, useNavigate } from 'react-router-dom';
 import Rating from '../components/Rating';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
-import { listProductsDetails } from '../actions/productActions';
+import {
+  createProductReview,
+  listProductsDetails,
+} from '../actions/productActions';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import { PRODUCT_REVIEW_CREATE_RESET } from '../constants/productConstants';
 
 const ProductScreen = () => {
   const dispatch = useDispatch();
@@ -23,14 +31,34 @@ const ProductScreen = () => {
   const productDetails = useSelector((state) => state.productDetails);
   const { loading, error, product } = productDetails;
 
+  const productReviewCreate = useSelector((state) => state.productReviewCreate);
+  const { success: successProductReview, error: errorProductReview } =
+    productReviewCreate;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   const [qty, setQty] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
 
   useEffect(() => {
+    if (successProductReview) {
+      alert('Review Submitted');
+      setRating(0);
+      setComment('');
+      dispatch({ type: PRODUCT_REVIEW_CREATE_RESET });
+    }
     dispatch(listProductsDetails(id));
-  }, [id, dispatch]);
+  }, [id, dispatch, successProductReview]);
 
   const addToCartHandler = () => {
     navigate(`/cart/${id}?qty=${qty}`);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(createProductReview(id, { rating, comment }));
   };
 
   return (
@@ -51,88 +79,159 @@ const ProductScreen = () => {
       ) : error ? (
         <Message type="error">{error}</Message>
       ) : (
-        <Grid templateColumns="5fr 4fr 3fr" gap="10">
-          {/**column 1 */}
-          <Image
-            src={product.image}
-            alt={product.name}
-            borderRadius="md"
-            border="solid black 1px"
-          />
-
-          {/**column 2 */}
-
-          <Flex direction="column">
-            <Heading as="h5" fontSize="base" color="gray.500">
-              {product.brand}
-            </Heading>
-
-            <Heading as="h2" fontSize="4xl" mb="5" color="red.900">
-              {product.name}
-            </Heading>
-
-            <Rating
-              value={product.rating}
-              color="yellow.500"
-              text={`${product.numReviews} reviews`}
+        <>
+          <Grid templateColumns="5fr 4fr 3fr" gap="10">
+            {/**column 1 */}
+            <Image
+              src={product.image}
+              alt={product.name}
+              borderRadius="md"
+              border="solid black 1px"
             />
 
-            <Heading
-              as="h5"
-              fontSize="4xl"
-              fontWeight="bold"
-              color="teal.700"
-              my="5"
-            >
-              ₹{product.price}
+            {/**column 2 */}
+
+            <Flex direction="column">
+              <Heading as="h5" fontSize="base" color="gray.500">
+                {product.brand}
+              </Heading>
+
+              <Heading as="h2" fontSize="4xl" mb="5" color="red.900">
+                {product.name}
+              </Heading>
+
+              <Rating
+                value={product.rating}
+                color="yellow.500"
+                text={`${product.numReviews} reviews`}
+              />
+
+              <Heading
+                as="h5"
+                fontSize="4xl"
+                fontWeight="bold"
+                color="teal.700"
+                my="5"
+              >
+                ₹{product.price}
+              </Heading>
+
+              <Text>{product.description}</Text>
+            </Flex>
+
+            {/**column 3 */}
+
+            <Flex direction="column">
+              <Flex justifyContent="space-between" py="2">
+                <Text>Price:</Text>
+                <Text fontWeight="bold">₹{product.price}</Text>
+              </Flex>
+
+              <Flex justifyContent="space-between" py="2" pb="10">
+                <Text>Status:</Text>
+                <Text fontWeight="bold">
+                  {product.countInStock > 0 ? 'In Stock' : 'Not available'}
+                </Text>
+              </Flex>
+
+              {product.countInStock > 0 && (
+                <Flex justifyContent="space-between" py="2">
+                  <Text>Qty:</Text>
+                  <Select
+                    value={qty}
+                    onChange={(e) => setQty(e.target.value)}
+                    width="30%"
+                  >
+                    {[...Array(product.countInStock).keys()].map((i) => (
+                      <option key={i + 1}>{i + 1}</option>
+                    ))}
+                  </Select>
+                </Flex>
+              )}
+
+              <Button
+                bg="red.900"
+                colorScheme="blackAlpha"
+                my="2"
+                textTransform="uppercase"
+                letterSpacing="wide"
+                isDisabled={product.countInStock === 0}
+                onClick={addToCartHandler}
+              >
+                Add to cart
+              </Button>
+            </Flex>
+          </Grid>
+
+          {/* Review */}
+          <Box
+            p="10"
+            bgColor="white"
+            rounded="md"
+            mt="10"
+            borderColor="gray.300"
+          >
+            <Heading as="h3" size="lg" mb="6">
+              Write a review
             </Heading>
 
-            <Text>{product.description}</Text>
-          </Flex>
+            {product.reviews.length === 0 && <Message>No Reviews</Message>}
 
-          {/**column 3 */}
-
-          <Flex direction="column">
-            <Flex justifyContent="space-between" py="2">
-              <Text>Price:</Text>
-              <Text fontWeight="bold">₹{product.price}</Text>
-            </Flex>
-
-            <Flex justifyContent="space-between" py="2" pb="10">
-              <Text>Status:</Text>
-              <Text fontWeight="bold">
-                {product.countInStock > 0 ? 'In Stock' : 'Not available'}
-              </Text>
-            </Flex>
-
-            {product.countInStock > 0 && (
-              <Flex justifyContent="space-between" py="2">
-                <Text>Qty:</Text>
-                <Select
-                  value={qty}
-                  onChange={(e) => setQty(e.target.value)}
-                  width="30%"
-                >
-                  {[...Array(product.countInStock).keys()].map((i) => (
-                    <option key={i + 1}>{i + 1}</option>
-                  ))}
-                </Select>
-              </Flex>
+            {product.reviews.length !== 0 && (
+              <Box p="4" bgColor="white" rounded="md" mb="1" mt="5">
+                {product.reviews.map((review) => (
+                  <Flex direction="column" key={review._id} mb="5">
+                    <Flex justifyContent="space-between">
+                      <Text fontSize="lg">
+                        <strong>{review.name}</strong>
+                      </Text>
+                      <Rating value={review.rating} />
+                    </Flex>
+                    <Text mt="2">{review.comment}</Text>
+                  </Flex>
+                ))}
+              </Box>
             )}
 
-            <Button
-              bg="red.900"
-              colorScheme="blackAlpha"
-              my="2"
-              textTransform="uppercase"
-              letterSpacing="wide"
-              isDisabled={product.countInStock === 0}
-              onClick={addToCartHandler}
-            >
-              Add to cart
-            </Button>
-          </Flex>
-        </Grid>
+            {errorProductReview && (
+              <Message type="error">{errorProductReview}</Message>
+            )}
+
+            {userInfo ? (
+              <form onSubmit={submitHandler}>
+                <FormControl id="rating" mb="3">
+                  <FormLabel>Rating</FormLabel>
+                  <Select
+                    placeholder="Select Option"
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                  >
+                    <option>Select...</option>
+                    <option value="1">1 - Poor</option>
+                    <option value="2">2 - Okay</option>
+                    <option value="3">3 - Good</option>
+                    <option value="4">4 - Very Good</option>
+                    <option value="5">5 - Excellent</option>
+                  </Select>
+                </FormControl>
+
+                <FormControl id="comment" mb="3">
+                  <FormLabel>Comment</FormLabel>
+                  <Textarea
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                  ></Textarea>
+                </FormControl>
+
+                <Button colorScheme="teal" type="submit">
+                  Post Review
+                </Button>
+              </form>
+            ) : (
+              <Message>Please login to write a review</Message>
+            )}
+          </Box>
+        </>
       )}
     </>
   );
